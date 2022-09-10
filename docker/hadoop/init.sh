ec3909fef3e365de036989ec661f2ab7988dc6fa
woynert <woynertgamer@gmail.com>
2022-09-09 23:31:24 -0500

    Split hadoop core processes

--- docker/hadoop/init.sh
+++ docker/hadoop/init.sh
@@ -1,10 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+
+FAIL=0
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+NC="\033[0m"
 
 # ssh daemon
 /usr/sbin/sshd 2>&1 &
 
-# hdfs
-/opt/hadoop-3.3.3/sbin/start-dfs.sh 2>&1 &
+# namenode
+/opt/hadoop-3.3.3/sbin/start-dfs-namenode.sh 2>&1 | while read line; do printf "$CYAN%s$NC\n" "[namenode] $line"; done &
+pidlist="$pidlist $!"
 
-# workaround to keep it alive
-/bin/sleep infinity
+# secondarynamenode
+/opt/hadoop-3.3.3/sbin/start-dfs-secondary.sh 2>&1 | while read line; do printf "$GREEN%s$NC\n" "[secondary] $line"; done &
+pidlist="$pidlist $!"
+
+# datanode
+#/opt/hadoop-3.3.3/sbin/custom-start-dfs-3.sh 2>&1 & | while read line; do echo "[] $line"; done
+
+# wait
+for job in $pidlist
+do
+	echo $job
+	wait $job || let "FAIL+=1"
+done
+
+# end
+if [ "$FAIL" == "0" ];
+then
+	echo "succeded ($FAIL)"
+else
+	echo "failed ($FAIL)"
+fi
+
+# jeremy.zawodny.com/blog/archives/010717.html
