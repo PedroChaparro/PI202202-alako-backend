50b24a77a49161efbbc45fd692ff4ff2cf788e66
PedroChaparro <pedro.chaparro.2020@upb.edu.co>
2022-09-27 14:00:49 -0500

    Save response as json array

--- scala/src/main/scala/cosine/cosine_similarity.scala
+++ scala/src/main/scala/cosine/cosine_similarity.scala
@@ -1,5 +1,6 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, Row}
 import org.apache.spark.sql.types._
+import play.api.libs.json.{JsArray, Json}
 
 object scala_test {
 
@@ -7,75 +8,105 @@ object scala_test {
 
 		// *** *** *** *** *** ***
 		// Create spark session and context
-		val sparksession = SparkSession.builder().appName("dot_product").master("local").getOrCreate()
-		val sc = sparksession.sparkContext;
+		val sparksession = SparkSession.builder().appName("dot_product").master("local[*]").getOrCreate()
+		val sc = sparksession.sparkContext
 
 		// *** *** *** *** *** ***
 		// Create accumulators
-		val dot_product = sc.doubleAccumulator("DotProductAccumulator");
-		val video_length = sc.doubleAccumulator("VideoLengthAccumulator");
-		val query_length = sc.doubleAccumulator("QueryLenghtAccumulator");
+		val dot_product = sc.doubleAccumulator("DotProductAccumulator")
+		val video_length = sc.doubleAccumulator("VideoLengthAccumulator")
+		val query_length = sc.doubleAccumulator("QueryLenghtAccumulator")
 
 		// *** *** *** *** *** ***
 		// Read json
-		val customSchema = StructType(Array(
+		val jsonSchema = StructType(Array(
 			StructField("url", StringType, false),
 			StructField("title", StringType, false),
-			StructField("description", StringType, false),
 			StructField("tags", StringType, false),
 			StructField("thumbnail", StringType, false),
 			StructField("vector", ArrayType(DoubleType, false), false)
 		))
 
-		val sparkDataFrame = sparksession.read.schema(customSchema)
+		val sparkDataFrame = sparksession.read.schema(jsonSchema)
 		  .option("multiline", true)
-		  .json("hdfs://hadoopmaster:9000/data/data.json")
+		  .json("/home/pedroch/Documents/github/PI202202-alako-data/data-cleaning/vectorized_data/data.json")
+
+
+		// Add cs column
+		// sparkDataFrame = sparkDataFrame.withColumn("cosine_similarity", lit(0.0: Double))
+
+		// sparkDataFrame.show()
 
 		// *** *** *** *** *** ***
 		// Read query from CLI
-		var query: Array[Double] = Array[Double]();
+		var query: Array[Double] = Array[Double]()
 
 		for (arg <- args) {
 			// Remove comma
 			if (arg.charAt(arg.length() - 1) == ',') {
-				query = query :+ arg.substring(0, arg.length() - 1).toDouble;
+				query = query :+ arg.substring(0, arg.length() - 1).toDouble
 			} else {
-				query = query :+ arg.toDouble;
+				query = query :+ arg.toDouble
 			}
 		}
 
 		// *** *** *** *** *** ***
 		// Algorithm
-		def CS(query: Array[Double]): Unit = {
-			sc.parallelize(sparkDataFrame.collect()).foreach(row => {
+		def CS(query: Array[Double]): JsArray = {
+
+			// Json response
+			var arr: JsArray = new JsArray()
+
+			// Testing with filter
+			// val rdd = sc.parallelize(sparkDataFrame.collect()
+
+			sparkDataFrame.collect().foreach(row => {
 				// Reset accumulators
-				dot_product.reset();
-				video_length.reset();
-				query_length.reset();
+				dot_product.reset()
+				video_length.reset()
+				query_length.reset()
 
 				// Get current video data
 				val currvid_title = row.getAs[String]("title")
-				val currvid_vector = row.getAs[scala.collection.mutable.ArraySeq[Double]]("vector").toArray;
+				val currvid_url = row.getAs[String]("url")
+				val currvid_tags =  row.getAs[String]("tags")
+				val currvid_thumbnail = row.getAs[String]("thumbnail")
+				val currvid_vector = row.getAs[scala.collection.mutable.ArraySeq[Double]]("vector").toArray
 
 				// Zipped function creates a collection of pairs
 				var ccs = (currvid_vector, query).zipped.map { (v, q) => {
-						dot_product.add(v * q);
-						video_length.add(v * v);
-						query_length.add(q * q);
+						dot_product.add(v * q)
+						video_length.add(v * v)
+						query_length.add(q * q)
 					}
 				}
 
 				// Get results
-				val cosine_similatiry = dot_product.value / (Math.sqrt(video_length.value) * Math.sqrt(query_length.value));
+				val cosine_similatiry = dot_product.value / (Math.sqrt(video_length.value) * Math.sqrt(query_length.value))
+				//sparkDataFrame = sparkDataFrame.withColumn("cosine_similarity", when(col("url") === currvid_url, cosine_similatiry).otherwise(col("cosine_similarity")))
 
 				if (cosine_similatiry >= 0.5) {
-					println("FIND!: " + currvid_title + " : " + Math.round(cosine_similatiry * 100) + "%");
+					//println("FIND!: " + currvid_title + " : " + Math.round(cosine_similatiry * 100) + "%")
+					val vid_object = Json.obj( // Create json object
+						"url" -> currvid_url,
+						"title" -> currvid_title,
+						"thumbnail" -> currvid_thumbnail,
+						"tags" -> currvid_tags,
+						"similarity" -> cosine_similatiry
+					)
+
+					arr = arr ++ Json.arr(vid_object)
 				}
 
 			})
+
+			return arr
+
 		}
 
-		CS(query);
+		val response: JsArray =  CS(query)
+		println(response)
+
 	}
 
 }
